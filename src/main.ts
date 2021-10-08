7e8f71853dd30b16e3b7f437b7610b9928b4a20c
fabsvas <fabricio.vasconc@gmail.com>
2021-10-08 00:42:08 -0300

    Add bootstrap framework css components

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {far} from '@fortawesome/free-regular-svg-icons';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {fab} from '@fortawesome/free-brands-svg-icons';
-
-library.add(far, fab, fas);
+import BootstrapVue3 from 'bootstrap-vue-3'
+import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import Maska from 'maska'
 
 createApp(App)
-  .component('fa', FontAwesomeIcon)
   .use(router)
+  .use(BootstrapVue3)
+  .use(BootstrapIconsPlugin)
+  .use(Maska)
   .mount('#app');
